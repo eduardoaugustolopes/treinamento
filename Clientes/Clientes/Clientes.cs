d2004b31eb9d5fa8787cc78c99291fcdb60dd63b
eduardoaugustolopes <eduardo@sommus.com>
2015-08-29 10:09:02 -0300

    Inclusão de Projeto

---
+++ Clientes/Clientes/Clientes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientes
+{
+    class Clientes
+    {
+        private int clienteId;
+
+        public int ClienteID
+        {
+            get { return clienteId; }
+            set { clienteId = value; }
+        }
+
+        private string clienteNome;
+
+        public string ClienteNome
+        {
+            get { return clienteNome; }
+            set { clienteNome = value; }
+        }
+
+        private string cnpjCliente;
+
+        public string CnpjCliente
+        {
+            get { return cnpjCliente; }
+            set { cnpjCliente = value; }
+        }
+
+
+    }
+}
